616f971e43e878edb142bdbbe15acadda26b3535
Matheus Aguiar <first_mf@hotmail.com>
2021-06-17 17:04:20 -0300

    created users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,94 @@
+import {
+  Icon,
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Checkbox,
+  Text,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th px="6" color="gray.300" width="8">
+                  Usuário
+                </Th>
+                <Th px="6" color="gray.300" width="8">
+                  Data de cadastro
+                </Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px="6">
+                  <Box>
+                    <Text fontWeight="bold">Matheus Aguiar</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      mfzaguiar@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text fontSize="sm">17 de junho de 2021</Text>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
