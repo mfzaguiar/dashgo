2aefa155944f1f1487ec263c04c633755c6f132d
Matheus Aguiar <first_mf@hotmail.com>
2021-06-18 18:23:58 -0300

    add link redirect using Next Link

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,6 +12,7 @@ import {
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input";
+import Link from "next/link";
 
 export default function CreateUser() {
   return (
@@ -44,7 +45,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
